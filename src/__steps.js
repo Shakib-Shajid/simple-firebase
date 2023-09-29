1b11c9f1e3cbc948edaec9acc35cff563978dd16
Shakib-Shajid <shakibalshajid1682@gmail.com>
2023-09-29 20:36:25 +0600

    authentication added

--- src/__steps.js
+++ src/__steps.js
@@ -5,5 +5,10 @@
  * 4. install firebase: npm install firebase
  * 5. add config file to your file
  * 6. Danger; Do not publish those in github
- *
+ * 7. Go to docs > Build > Authentication > Web > Get Started
+ * 8. export app from the firebase.config.js file: export default app
+ * 9. Login.jsx: import getAuth from firebase.auth
+ * 10. crate const auth = getAuth(app)
+ * 11. import googleAuthProvider and create new Provider
+ * 12. use signInWithPopUp and pass auth and provider
  */
